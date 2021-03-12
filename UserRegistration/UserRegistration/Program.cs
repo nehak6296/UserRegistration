51a6cdc2dea28a8567708ce65579a74a6cd503d0
nehak6296 <nehak6296@gmail.com>
2021-03-12 17:00:33 +0530

    [Neha]Add:Validating Phone number

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -10,6 +10,7 @@ namespace UserRegistration
             Console.WriteLine("Your First Name Match :" + validateUserDetails.validateFirstName("Neha"));
             Console.WriteLine("Your Last Name Match :" + validateUserDetails.validateLastName("Kotarwar"));
             Console.WriteLine("Your Mail Match :" + validateUserDetails.validateEmail("abc.xyz@bl.co.in"));
+            Console.WriteLine("Your Phone Number Match :" + validateUserDetails.validatePhoneNumber("91 9919819801"));
         }
     }
 }
--- UserRegistration/UserRegistration/ValidateUserDetails.cs
+++ UserRegistration/UserRegistration/ValidateUserDetails.cs
@@ -9,8 +9,8 @@ namespace UserRegistration
     {
         public static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
         public static string REGEX_LASTNAME = "^[A-Z][a-z]{2,}$";
-
         public static string REGEX_EMAIL = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        public static string REGEX_PHONENUMBER = "^(91[ ])?[7-9][0-9]{9}$"; //91 9919819801
         public bool validateFirstName(string firstName)
         {           
             return Regex.IsMatch(firstName , REGEX_FIRSTNAME);
@@ -23,6 +23,10 @@ namespace UserRegistration
         public bool validateEmail(string mail)
         {
             return Regex.IsMatch(mail, REGEX_EMAIL);
+        }
+        public bool validatePhoneNumber(string phoneNumber)
+        {
+            return Regex.IsMatch(phoneNumber, REGEX_PHONENUMBER);
         }
     }
 }
