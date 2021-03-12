7f981f3b95b2e1d408bb5a81e66f969d5c34bf08
nehak6296 <nehak6296@gmail.com>
2021-03-12 18:47:43 +0530

    [Neha]Add:Checking password for at least 1 uppper case

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -11,7 +11,7 @@ namespace UserRegistration
             Console.WriteLine("Your Last Name Match :" + validateUserDetails.validateLastName("Kotarwar"));
             Console.WriteLine("Your Mail Match :" + validateUserDetails.validateEmail("abc.xyz@bl.co.in"));
             Console.WriteLine("Your Phone Number Match :" + validateUserDetails.validatePhoneNumber("91 9919819801"));
-            Console.WriteLine("Your Password Match :" + validateUserDetails.validatePassword("Neha@1234"));
+            Console.WriteLine("Your Password Match :" + validateUserDetails.validatePassword("Neha#4568"));
 
         }
     }
