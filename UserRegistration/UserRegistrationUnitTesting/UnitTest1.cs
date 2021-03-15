175ba971e96e7945563a48731b526dd1fbe2ba60
nehak6296 <nehak6296@gmail.com>
2021-03-15 22:23:23 +0530

    [Neha]Added:Test for validating last name

--- UserRegistration/UserRegistrationUnitTesting/UnitTest1.cs
+++ UserRegistration/UserRegistrationUnitTesting/UnitTest1.cs
@@ -6,14 +6,29 @@ namespace UserRegistrationUnitTesting
     [TestClass]
     public class UnitTest1
     {
+        //Arrange
         ValidateUserDetails validateUserDetails = new ValidateUserDetails();
+        bool expectedResult = true;
 
         [TestMethod]
         public void Test_First_Name()
         {
-            bool expectedResult = true;
+            //Act        
             bool result = validateUserDetails.validateFirstName("Neha");
+
+            //Assert
             Assert.AreEqual(expectedResult,result);
+        }
+
+        [TestMethod]
+
+        public void Test_Last_Name()
+        {
+            //Act
+            bool result = validateUserDetails.validateLastName("Kotarwar");
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
         }
     }
 }
